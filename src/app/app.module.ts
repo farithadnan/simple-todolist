96467ffefb138dd425982dac4537ade44f9b71ff
Farith <22324133+farithadnan@users.noreply.github.com>
2023-02-20 23:14:19 +0800

    Add basic UI for to-do list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+
+
+import { TodolistComponent } from './pages/todolist/todolist.component';
+
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +42,15 @@ registerLocaleData(en);
     AppRoutingModule,
     IconsProviderModule,
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzGridModule,
+    NzCardModule,
+    NzFormModule,
+    NzInputModule,
+    NzSpaceModule,
+    NzButtonModule,
+    NzDatePickerModule,
+    NzTableModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
--- src/app/pages/todolist/todolist.module.ts
+++ src/app/pages/todolist/todolist.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodolistRoutingModule } from './todolist-routing.module';
-import { TodolistComponent } from './todolist.component';
 
 
 @NgModule({
